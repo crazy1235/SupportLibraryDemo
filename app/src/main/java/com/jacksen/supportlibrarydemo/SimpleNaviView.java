76e175b1778d6f5c95f2b7ab42206d6149eac5f0
jacksen <951532416@qq.com>
2016-03-01 23:17:49 +0800

    update navigation view

---
+++ app/src/main/java/com/jacksen/supportlibrarydemo/SimpleNaviView.java
@@ -0,0 +1,18 @@
+package com.jacksen.supportlibrarydemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * simple navigation view demo
+ *
+ * @author jacksen
+ */
+public class SimpleNaviView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_navi_view);
+    }
+}

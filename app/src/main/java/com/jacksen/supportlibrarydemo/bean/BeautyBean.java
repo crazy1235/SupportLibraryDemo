7716956cae054411c93f2a5806d120fa619bf51e
jacksen <yansen19920101@gmail.com>
2017-03-11 18:39:42 +0800

    update

--- app/src/main/java/com/jacksen/supportlibrarydemo/bean/BeautyBean.java
+++ app/src/main/java/com/jacksen/supportlibrarydemo/bean/BeautyBean.java
@@ -1,15 +1,36 @@
 package com.jacksen.supportlibrarydemo.bean;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by jacksen on 2016/3/3.
  */
-public class BeautyBean {
+public class BeautyBean implements Parcelable{
 
     private int imageId;
 
     private String name;
 
     private String desc;
+
+    protected BeautyBean(Parcel in) {
+        imageId = in.readInt();
+        name = in.readString();
+        desc = in.readString();
+    }
+
+    public static final Creator<BeautyBean> CREATOR = new Creator<BeautyBean>() {
+        @Override
+        public BeautyBean createFromParcel(Parcel in) {
+            return new BeautyBean(in);
+        }
+
+        @Override
+        public BeautyBean[] newArray(int size) {
+            return new BeautyBean[size];
+        }
+    };
 
     public int getImageId() {
         return imageId;
@@ -53,5 +74,17 @@ public class BeautyBean {
     public BeautyBean(String name, String desc) {
         this.name = name;
         this.desc = desc;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(imageId);
+        dest.writeString(name);
+        dest.writeString(desc);
     }
 }

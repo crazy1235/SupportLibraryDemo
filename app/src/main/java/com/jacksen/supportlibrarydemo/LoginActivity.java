9909678088855510dd9769e90b895a3bdc38e06f
jacksen <yansen19920101@gmail.com>
2017-07-24 13:53:09 +0800

    update

--- app/src/main/java/com/jacksen/supportlibrarydemo/LoginActivity.java
+++ app/src/main/java/com/jacksen/supportlibrarydemo/LoginActivity.java
@@ -53,7 +53,7 @@ public class LoginActivity extends AppCompatActivity {
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                if (id == EditorInfo.IME_NULL) {
                     attemptLogin();
                     return true;
                 }
